a216fcf7c6788a762ad7825d9835c36f2a12f271
Arif BAĞLAR <arifbaglar@hotmail.com>
2019-06-05 03:56:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (blok) {
+  return (42-blok)
+}
+ 
+function distanceFromHqInFeet (blok) {
+  distanceFromHqInBlocks(someValue);
+  
+}
